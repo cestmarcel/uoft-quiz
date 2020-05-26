2ecccbdf22338f1edb9428d02826b73de08d6c94
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-05-25 23:31:36 -0400

    Set up core html elements and created javascript around starting the game

---
+++ script.js
@@ -0,0 +1,159 @@
+// Defining questions
+
+var questions = [
+    {
+        q: "What HTML element does your JavaScript go into?",
+        a: ["<js>", "<javascript>", "<script>", "<other>"],
+        v: 2
+    },
+    {
+        q: "How do you start a FOR loop?",
+        a: ["for(i=0; i<3; i++", "FOR loop < 5", "for(loop i times, i = 5)", "<for i=5>"],
+        v: 0
+    },
+    {
+        q: "How do you open a single-line comment in JavaScript?",
+        a: ["<!--", "//", "<comment>", ".comment"],
+        v: 1
+    },
+    {
+        q: "How do you correctly refer to an external script called script.js?",
+        a: ["<script src=\"script.js\"></script>", "<javascript src=\"script.js\"></javascript>", "<script href=\"script.js\"></script>", "<script \"script.js\"></script>"],
+        v: 0
+    },
+    {
+        q: "In order to create a function in JavaScript, you have to type ______ :",
+        a: ["function doSomething(){", "new:function", "<start>myFunction()", "<function>newFunction</function>"],
+        v: 0
+    },
+    {
+        q: "How do you correctly round the number 8.645 to the nearest whole number (integer)?",
+        a: ["round(8.645)", "Math.ceil(8.645)", "roundToInteger: 8.645", "Math.round(8.645)"],
+        v: 3
+    },
+    {
+        q: "Pick the correct syntax to change the content of the following HTML element: <p id=\"example\">I am a bit of copy</p>",
+        a: ["document.querySelector(\"#example\").textContent = \"Hi!\"", "document.getElementsByClassName(\"example\").textContent = \"Hi!\"", "file.querySelector(\"#example\").textContent = \"Hi!\"", "document.querySelector(\"#example\").changeText = \"Hi!\""],
+        v: 0
+    },
+    {
+        q: "How would you correctly alert \"Hi!\" to the user using JavaScript?",
+        a: ["<alert>Hi!</alert>", "alert: \"Hi!\"", "alert(\"Hi!\")", "function{alert = \"Hi!\""],
+        v: 2
+    },
+    {
+        q: "How do you call a function \"startGame()\" in JavaScript?",
+        a: ["call: startGame", "startGame", "startGame();", "callFunction=startGame;"],
+        v: 2
+    },
+    {
+        q: "How do you open a multi-line comment in JavaScript?",
+        a: ["<!--", "//", "Multi-line comments are not possible in JavaScript", "/*"],
+        v: 3
+    },
+    {
+        q: "How do you correctly create a new variable called \"star\" and set its value to \"0\"?",
+        a: ["var star = 0;", "newVariable(star): 0;", "create: star = 0:", "star(0)"],
+        v: 0
+    },
+    {
+        q: "Your JavaScript code in an external JavaScript file has to be wrapped in <script></script>:",
+        a: ["Only if it has more than 50 lines of code", "No, it does not", "Yes, otherwise it will not work", "The <script> tag does not exist"],
+        v: 1
+    },
+    {
+        q: "Which of the following is an array?",
+        a: ["var array = 1, 2, 3;", "var array = [1, 2, 3];", "array: 1, 2, 3", "array.1 [1], array.2 [2], array.3 [3]"],
+        v: 1
+    },
+    {
+        q: "If you choose to include JavaScript in your HTML file, what is the place to do that?",
+        a: ["<head>", "<footer>", "<html>", "<body>"],
+        v: 3
+    },
+    {
+        q: "Which of the following characters indicate that you're dealing with an object?",
+        a: ["<object>", "<>", "{}", "<!-- -->"],
+        v: 2
+    },
+    {
+        q: "What is the correct syntax to get a random full number (integer) between 0 and 3?",
+        a: ["Math.round(Math.random() * 3)", "Math.round(Math.random() * 4)", "Math.random(4)", "randomNumber < 4"],
+        v: 0
+    },
+    {
+        q: "What does the following code do? animals[5]",
+        a: ["Creates a new variable called \"animals\" and sets its value to 5", "Picks the fifth element from the \"animals\" array", "Picks the sixth element from the \"animals\" array", "Creates five variables called \"animals\""],
+        v: 2
+    },
+    {
+        q: "Which of these if statements is correct?",
+        a: ["if(tip < .15){alert(\"Hi!\")}", "if<tip < .15>{alert(\"Hi!\")}", "if{tip < .15}=alert(\"Hi!\")", "IF:tip < .15 THEN: alert(\"Hi!\")"],
+        v: 0
+    },
+    {
+        q: "What does the following code do? console.log(example)",
+        a: ["Prints \"example\" to the console", "Prints the value of the variable \"example\" to the console", "Saves the value of the variable \"example\" to the clipboard", "Nothing, this is invalid code in JavaScript"],
+        v: 1
+    },
+    {
+        q: "What is JavaScript?",
+        a: ["An API", "A computer program", "A word to describe any script in any file", "A programming language"],
+        v: 3
+    }
+]
+
+// Set up the timer
+
+var timer = 60
+
+function startTimer(){
+var gameTimer = setInterval(function(){
+    timer -= 1;
+    if (timer <= 0){
+        clearInterval(gameTimer);
+    }
+    document.getElementById("timer").textContent = `Time left: ${timer}s`;
+}, 1000);
+}
+
+// Set up question loop
+
+var i = 0;
+var score = 0;
+
+function askFirstQuestion(){
+    document.getElementById("q").textContent = questions[i].q;
+    document.getElementById("a1").textContent = questions[i].a[0];
+    document.getElementById("a2").textContent = questions[i].a[1];
+    document.getElementById("a3").textContent = questions[i].a[2];
+    document.getElementById("a4").textContent = questions[i].a[3];
+    document.getElementById("question-counter").textContent = `Question ${i+1}`;
+    }
+
+function askQuestion(number){
+    var choice = number;
+    if(choice == questions[i].v){
+        document.getElementById("feedback").style = "display: block";
+        document.getElementById("feedback").textContent = "Correct!";
+        score++;
+    } else {
+        document.getElementById("feedback").style = "display: block";
+        document.getElementById("feedback").textContent = "Wrong!";
+        timer = timer-5;
+    }
+}
+
+// Start the game
+
+function startGame(){
+    document.getElementById("welcome-zone").style = "display: none;";
+    document.getElementById("question").style = "display: block;";
+    document.getElementById("timer").style = "display: block";
+    document.getElementById("header-placeholder").style = "display: none";
+    startTimer();
+    askFirstQuestion();
+}
+
+document.getElementById("startgame").addEventListener("click", startGame);
+
